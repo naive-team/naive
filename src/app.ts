b4c5747dcad05f98985c05f459f810346e7e4bda
Bj-LAI <bjornlai13@gmail.com>
2025-12-20 21:28:45 -0500

    Initialisation de l'inputManager

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
 import "@babylonjs/core/Debug/debugLayer";
 import "@babylonjs/inspector";
-import { Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight, Mesh, MeshBuilder } from "@babylonjs/core";
-import {KeyboardInput} from "./Input/KeyboardInput";
-import {InputStrategy} from "./Input/InputStrategy";
+import { Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight, Mesh, MeshBuilder, GamepadManager, GenericPad } from "@babylonjs/core";
+import {InputStrategy} from "./Input/InputStrategies/InputStrategy";
+import {GenericGamepadInput} from "./Input/InputStrategies/GenericGamepadInput";
+import {InputManager} from "./Input/InputManager";
 
 class App {
     constructor() {
@@ -12,18 +13,13 @@ class App {
         canvas.style.height = "100%";
         canvas.id = "gameCanvas";
         document.body.appendChild(canvas);
-
         // initialize babylon scene and engine
         var engine = new Engine(canvas, true);
         var scene = new Scene(engine);
-
         var camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), scene);
-        camera.attachControl(canvas, true);
+
         var light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
         var sphere: Mesh = MeshBuilder.CreateSphere("sphere", { diameter: 1 }, scene);
-
-        const input: InputStrategy = new KeyboardInput().attach();
-
         // hide/show the Inspector
         window.addEventListener("keydown", (ev) => {
             // Shift+Ctrl+Alt+I
@@ -36,10 +32,13 @@ class App {
             }
         });
 
+
+        const inputManager: InputManager = new InputManager();
+
         // run the main render loop
         engine.runRenderLoop(() => {
             scene.render();
-            input.update();
+            inputManager.update();
         });
     }
 }

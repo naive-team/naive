eccd15a8d563338276e59c1d0dad9751608e0999
RyoLucifer <florentgilloteaux@gmail.com>
2026-01-29 23:23:36 -0500

    Renommage attributs

--- src/Player.ts
+++ src/Player.ts
@@ -1,12 +1,12 @@
 import {Mesh, TransformNode} from "@babylonjs/core";
 
 export class Player extends TransformNode {
-    private mesh: Mesh;
+    private mesh_: Mesh;
 
     public constructor(mesh: Mesh) {
         super("Player");
-        this.mesh = mesh;
-        this.mesh.setParent(this);
+        this.mesh_ = mesh;
+        this.mesh_.setParent(this);
 
     }
 }

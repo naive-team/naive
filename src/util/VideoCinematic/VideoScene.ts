4bd642891e34b939b903a455035907a88403da6c
Bj-LAI <bjornlai13@gmail.com>
2026-04-30 13:31:29 +0200

    videoScene class to play a video

---
+++ src/util/VideoCinematic/VideoScene.ts
@@ -0,0 +1,33 @@
+import { Engine, HemisphericLight, Scene, UniversalCamera, Vector3 } from "@babylonjs/core";
+import { VideoMesh } from "./VideoMesh";
+import { IScene } from "../IScene";
+
+export class VideoScene extends Scene implements IScene {
+    private video_: VideoMesh;
+    private camera_: UniversalCamera;
+    private isReady_: boolean;
+
+    constructor(engine: Engine, name: string, src: string | string[] | HTMLVideoElement, cameraZ?: number, onLoaded?: () => void, onEnded?: () => void) {
+        super(engine);
+
+        const camZ = cameraZ ? cameraZ : -1.18;
+
+        this.video_ = new VideoMesh(name, src, this, onLoaded, onEnded);
+        this.camera_ = new UniversalCamera("camera", new Vector3(0, 0, camZ), this);
+
+        const light: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), this);
+
+    }
+
+    start(): boolean {
+        if (! this.isReady_) return false;
+        this.video_.start();
+
+        return true;
+    }
+
+    public async waitUntilReady(): Promise<void> {
+        await this.video_.waitUntilReady();
+        this.isReady_ = true;
+    }
+}   

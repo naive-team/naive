87246e470ad399f27b956fbd72969c2e3ae4d1d6
laiz3272 <zia.lai@usherbrooke.ca>
2026-05-28 10:48:09 -0400

    feat: correction sous titre intro

--- src/Scenes/ActivezLeSon.ts
+++ src/Scenes/ActivezLeSon.ts
@@ -19,7 +19,7 @@ export class ActivezLeSon extends Scene implements AsyncScene {
         this.onNextScene_ = async () => {
             const videoScene = new VideoScene(engine,
                 "intro",
-                "./intro corigee.mp4",
+                "./intro corrigée2.mp4",
                 -1.18,
                 ()=>{ },
                 async() => {await sceneManager.switchTo(new LabScene(this.engine_, this.sceneManager_))

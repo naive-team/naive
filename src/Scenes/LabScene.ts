60723d328bdd9411840879420a94405c8a525d8c
laiz3272 <zia.lai@usherbrooke.ca>
2026-05-29 10:37:38 -0400

    nouvelle version de lab

--- src/Scenes/LabScene.ts
+++ src/Scenes/LabScene.ts
@@ -129,7 +129,7 @@ export class LabScene extends Scene implements AsyncScene {
         this.playerMesh_ = playerMeshData.mesh;
         this.playerMesh_.checkCollisions = true;
         this.playerAnimations_ = playerMeshData.animationGroups;
-        const labMeshData = await MeshLoader.loadMesh("./lab.glb", this);
+        const labMeshData = await MeshLoader.loadMesh("./lab-2.glb", this);
         this.labMesh = labMeshData.mesh;
 
         const calimeshdata = await MeshLoader.loadMesh("./cali.glb", this);
